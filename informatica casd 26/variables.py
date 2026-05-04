c0745d5351ba7c2f574d654f89a1f326966b73eb
Sawtinted <andrwss00@gmail.com>
2026-05-04 11:03:26 -0500

    primer ejercicio con python
    Empezando a trabajar con Python, aprendiendo sobre comentarios, variables, funcion print, funcion input, funcion type y concatenacion.

---
+++ informatica casd 26/variables.py
@@ -0,0 +1,22 @@
+#Esto es un comentario de una sola linea
+"""Esto es un comentario de 
+varias lineas"""
+
+#inicializando variables
+nombre="nombre del estudiante"
+edad=58
+estado=True
+nota=5.0
+
+#mostrar el contenido de los variables print()
+print(nombre)
+print(edad)
+print(edad)
+print(estado)
+print(nota)
+
+#que tipo de dato contiene cada variable.
+print(type(nombre))
+print(type(edad))
+print(type(estado))
+print(type(nota))
